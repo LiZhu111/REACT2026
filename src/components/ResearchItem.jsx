186c833836c113efde049ffbd48e5986a0055a4b
LiZhu <lzhu83168@gmail.com>
2026-02-19 23:11:12 +1100

    Increase spacing of 'Explore More Research' button for better layout consistency with Member section

--- src/components/ResearchItem.jsx
+++ src/components/ResearchItem.jsx
@@ -74,9 +74,9 @@ export default function ResearchItem({
         </div>
       </div>
 
-      {/* 优化后的加载更多按钮：在小屏增加 mt-20 防止拥挤 */}
+      {/* 优化后的加载更多按钮：在小屏增加 mt-32 防止拥挤，与 Member 部分保持一致 */}
       {isLastVisible && hasMore && (
-        <div className="relative mt-20 pb-12 lg:absolute lg:bottom-12 lg:left-1/2 lg:-translate-x-1/2 lg:mt-0 lg:pb-0 z-50 w-full flex justify-center">
+        <div className="relative mt-32 pb-12 lg:absolute lg:bottom-12 lg:left-1/2 lg:-translate-x-1/2 lg:mt-0 lg:pb-0 z-50 w-full flex justify-center">
             <button onClick={onLoadMore} className="group flex flex-col items-center gap-4">
             <span className="text-[10px] tracking-[0.5em] text-cyan-400/40 group-hover:text-cyan-400 uppercase font-[200]">
                 {exploreText}
